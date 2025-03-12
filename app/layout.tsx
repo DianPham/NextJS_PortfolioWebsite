b7d0ab61e3124598164328819278f3b675de974d
DianPham <152237146+DianPham@users.noreply.github.com>
2025-03-12 21:48:01 +0700

    remove outfit font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Outfit , Ovo} from "next/font/google";
 import "./globals.css";
 
-const outfit = Outfit({
-  subsets: ["latin","latin-ext"], weight: ["400", "500", "600", "700"]
-});
 
 const ovo = Ovo({
   subsets: ["latin"], weight: ["400"]
